6aff59ea873b869f0aefdb7533ac913fec28705a
mariovernetti <mariovernett@gmail.com>
2017-03-10 11:44:30 +0100

    Add files via upload

---
+++ conta_giorni.cpp
@@ -0,0 +1,48 @@
+/*	          	conta_giorni.cpp		    *
+ *-----------------------------------------*
+ *				   File MAIN		        *
+ *           main di "conta_numeri"         *  
+ *-----------------------------------------*
+ *					                        */
+
+#include "stdafx.h"
+#include <iostream>
+#include <vector>
+#include <string>
+
+#include "struct.cpp"
+#include "utilities_func.h"
+
+
+
+using namespace System;
+using namespace std;
+	
+
+
+void main(void)
+{
+	DATE date1;
+	DATE date2; 
+
+
+    string giorno;
+	string mese;
+	string anno;
+
+	string a, b;
+
+	cout<<"Inserisci la prima data: ";cin>>a;
+	split_date(a);
+
+	cout<<giorno;
+	
+
+
+	
+	
+	
+	
+	Console::ReadKey();
+    
+}
---
+++ utilities_func.h
@@ -0,0 +1,19 @@
+#ifndef DATE_H
+#define DATE_H
+
+
+#include <string>
+#include <vector>
+#include <iostream>
+
+
+using namespace std;
+
+
+
+void split_date(string date_string);
+
+
+
+
+#endif //DATE_H
